468860e4187f06a02f69dc45767a5680d717a3ad
rt0112358 <rt0112358@gmail.com>
2020-09-18 17:20:52 -0600

    init okay DB

---
+++ okay.sql
@@ -0,0 +1,30 @@
+-- Init database to count number of times okay said
+CREATE TABLE okay (
+    num INT NOT NULL AUTO_INCREMENT, -- number of times okay said
+    date DATETIME, -- date and time okay said/
+    PRIMARY KEY (num)
+);
+
+-- init table data / example
+insert into okay (date) values (CURRENT_TIMESTAMP);
+
+
+-- Loop to autofill table with 100 entries
+DELIMITER $$
+
+CREATE PROCEDURE FillData()
+BEGIN
+
+    DECLARE i INT DEFAULT 1;
+
+    WHILE (i<=100) DO
+        INSERT INTO okay(date) VALUES(CURRENT_TIMESTAMP);
+        SET i=i+1;
+    END WHILE;
+
+END$$
+
+DELIMITER ;
+
+-- Execute the function
+call FillData();
